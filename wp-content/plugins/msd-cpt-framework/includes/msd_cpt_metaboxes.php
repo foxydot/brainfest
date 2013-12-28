49f3999012cc8973e55633294332fad8b4a32945
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2013-12-28 16:36:13 -0500

    subtitle fix

--- wp-content/plugins/msd-cpt-framework/includes/msd_cpt_metaboxes.php
+++ wp-content/plugins/msd-cpt-framework/includes/msd_cpt_metaboxes.php
@@ -1,17 +1,17 @@
 <?php
 global $subtitle,$documents;
+    // add a custom meta box
+    $documents = new WPAlchemy_MetaBox(array
+    (
+        'id' => '_documents',
+        'title' => 'Documents',
+        'types' => array('msd_publication','msd_success','msd_news'), // added only for pages and to custom post type "events"
+        'context' => 'normal', // same as above, defaults to "normal"
+        'priority' => 'high', // same as above, defaults to "high"
+        'template' => $msd_cpt_path.'/template/attach_file.php'
+    ));
 	// add a custom meta box
-	$documents = new WPAlchemy_MetaBox(array
-	(
-		'id' => '_documents',
-		'title' => 'Documents',
-		'types' => array('msd_publication','msd_success','msd_news'), // added only for pages and to custom post type "events"
-		'context' => 'normal', // same as above, defaults to "normal"
-		'priority' => 'high', // same as above, defaults to "high"
-		'template' => $msd_cpt_path.'/template/attach_file.php'
-	));
-	// add a custom meta box
-	$success = new WPAlchemy_MetaBox(array
+	$subtitle = new WPAlchemy_MetaBox(array
 	(
 		'id' => '_success',
 		'title' => 'Success Stories Subtitle',
