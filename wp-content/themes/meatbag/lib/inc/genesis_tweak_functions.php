7a24cffdbe0a7b06340c18e15085ed1533f52497
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-11-26 12:14:26 -0500

    remove posted by dates

--- wp-content/themes/meatbag/lib/inc/genesis_tweak_functions.php
+++ wp-content/themes/meatbag/lib/inc/genesis_tweak_functions.php
@@ -334,7 +334,7 @@ class Description_Walker extends Walker_Nav_Menu
  */
 function msdlab_do_social_footer(){
     global $msd_social;
-    
+    //hook_test('genesis_entry_header');
     if($msd_social){
         $address = '<span itemprop="name">'.$msd_social->get_bizname().'</span> | <span itemprop="streetAddress">'.get_option('msdsocial_street').'</span>, <span itemprop="streetAddress">'.get_option('msdsocial_street2').'</span> | <span itemprop="addressLocality">'.get_option('msdsocial_city').'</span>, <span itemprop="addressRegion">'.get_option('msdsocial_state').'</span> <span itemprop="postalCode">'.get_option('msdsocial_zip').'</span> | '.$msd_social->get_digits();
         $copyright .= '&copy; '.date('Y').' '.$msd_social->get_bizname().' | All Rights Reserved';
--- wp-content/themes/meatbag/lib/inc/genesis_tweaks.php
+++ wp-content/themes/meatbag/lib/inc/genesis_tweaks.php
@@ -34,7 +34,7 @@ add_filter('widget_text', 'do_shortcode');//shortcodes in widgets
 /*** CONTENT ***/
 add_filter('genesis_breadcrumb_args', 'msdlab_breadcrumb_args'); //customize the breadcrumb output
 remove_action('genesis_before_loop', 'genesis_do_breadcrumbs'); //move the breadcrumbs 
-add_filter( 'genesis_post_info', 'sp_post_info_filter' );
+//add_filter( 'genesis_post_info', 'sp_post_info_filter' );
 
 add_action('genesis_before_content_sidebar_wrap', 'genesis_do_breadcrumbs'); //to outside of the loop area
 add_action('genesis_before_entry','msd_post_image');//add the image above the entry
@@ -42,9 +42,9 @@ add_action('genesis_before_entry','msd_post_image');//add the image above the en
 add_filter('excerpt_more', 'sp_read_more_link');
 add_filter( 'the_content_more_link', 'sp_read_more_link' );
 
-//remove_action( 'genesis_before_post_content', 'genesis_post_info' ); //remove the info (date, posted by,etc.)
+remove_action( 'genesis_before_post_content', 'genesis_post_info' ); //remove the info (date, posted by,etc.)
 remove_action( 'genesis_after_post_content', 'genesis_post_meta' ); //remove the meta (filed under, tags, etc.)
-//remove_action( 'genesis_entry_header', 'genesis_post_info' ); //remove the info (date, posted by,etc.)
+remove_action( 'genesis_entry_header', 'genesis_post_info',12 ); //remove the info (date, posted by,etc.)
 remove_action( 'genesis_entry_footer', 'genesis_post_meta'); //remove the meta (filed under, tags, etc.)
 //add_action( 'msdlab_title_area', 'msdlab_do_post_subtitle' );
 
